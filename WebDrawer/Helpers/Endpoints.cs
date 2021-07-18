b9fb00dcab911df054bdd1a4b611090315c75d9d
Yusuf Khan <yusufddkhan@gmail.com>
2021-07-18 14:44:49 +0200

    Added in pokemon object

---
+++ WebDrawer/Helpers/Endpoints.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDrawer.Helpers
+{
+    public class Endpoints
+    {
+        public string pokemonUrl { get; set; } = "https://pokeapi.co/api/v2/pokemon/";
+    }
+}
