8e0459faeaaa14a39c45f6b1d3dacd0a3d49ae24
Yusuf Khan <yusufddkhan@gmail.com>
2021-07-20 21:32:01 +0200

    Updated styles

--- WebDrawer/Controllers/HomeController.cs
+++ WebDrawer/Controllers/HomeController.cs
@@ -3,8 +3,6 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using WebDrawer.Data;
 using WebDrawer.Models;
 
